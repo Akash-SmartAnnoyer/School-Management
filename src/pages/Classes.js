afd7cadaec285a9d9b54615022515b2436e9a4ad
Akash-SmartAnnoyer <smartannoyerakash@gmail.com>
2025-09-14 11:40:54 +0530

    update classes

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -21,6 +21,24 @@ const { Search } = Input;
 
 const sections = ['A', 'B', 'C', 'D', 'E', 'F'];
 
+const gradeOptions = [
+  { value: 'Nursery', label: 'Nursery' },
+  { value: 'LKG', label: 'LKG (Lower Kindergarten)' },
+  { value: 'UKG', label: 'UKG (Upper Kindergarten)' },
+  { value: '1st', label: '1st Grade' },
+  { value: '2nd', label: '2nd Grade' },
+  { value: '3rd', label: '3rd Grade' },
+  { value: '4th', label: '4th Grade' },
+  { value: '5th', label: '5th Grade' },
+  { value: '6th', label: '6th Grade' },
+  { value: '7th', label: '7th Grade' },
+  { value: '8th', label: '8th Grade' },
+  { value: '9th', label: '9th Grade' },
+  { value: '10th', label: '10th Grade' },
+  { value: '11th', label: '11th Grade' },
+  { value: '12th', label: '12th Grade' }
+];
+
 const Classes = forwardRef((props, ref) => {
   const messageApi = useMessage();
   const { 
@@ -624,9 +642,22 @@ const Classes = forwardRef((props, ref) => {
                   <Form.Item
                     name="className"
                     label="Class Name"
-                    rules={[{ required: true, message: 'Please enter class name' }]}
+                    rules={[{ required: true, message: 'Please select class name' }]}
                   >
-                    <Input prefix={<BookOutlined style={{ color: '#bfbfbf' }} />} />
+                    <Select 
+                      placeholder="Select class name"
+                      showSearch
+                      optionFilterProp="children"
+                      filterOption={(input, option) =>
+                        option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                      }
+                    >
+                      {gradeOptions.map(grade => (
+                        <Option key={grade.value} value={grade.value}>
+                          {grade.label}
+                        </Option>
+                      ))}
+                    </Select>
                   </Form.Item>
 
                   <Form.Item
